a4e6184fbbbc8d47328f041bf471bd4f55225e51
Piyali Mukherjee <piyali.mukherjee.30.pm@gmail.com>
2015-04-29 23:39:29 -0400

    Begins with influencers (on current user)
    By-passing authentication problems for now, and keeping segregated data
    for additional analytics

--- reach.java
+++ reach.java
@@ -20,38 +20,40 @@ public class Reach {
     /**
      * @param args the command line arguments
      */
-    static ArrayList<String> user_original_content = new ArrayList<String>();
+    static ArrayList<String> user_original_timeline = new ArrayList<String>();
+    static ArrayList<String> humans_retweet_me = new ArrayList<String>();
+    static ArrayList<String> humans_directlymention_me = new ArrayList<String>();
+    static ConfigurationBuilder cb = new ConfigurationBuilder();
+    static TwitterFactory twitter_factory;
+    static Twitter twitter;
 
     public static void main(String[] args) throws TwitterException {
-        // TODO code application logic here
-        Reach obj = new Reach();
-        obj.GetHomeTimeline();
-        obj.debug_output();
+            cb.setDebugEnabled(true)
+                    .setOAuthConsumerKey("0fAuoIn3FSkptsdzlLIrg4MDm")
+                    .setOAuthConsumerSecret("F3CDyV0VGbeofhk87svXuLCZ0CJOryzuAa3Jgj1Z2P9iBPoTFq")
+                    .setOAuthAccessToken("2875358986-Xjv7ks8AL1X0cu0TVXbjryjxw52bNMZacJIQddW")
+                    .setOAuthAccessTokenSecret("hmHjTGWux0eYHIGb3YHX34C8f5eQZ81u0QDHkhVIPi9ua");
+            twitter_factory = new TwitterFactory(cb.build());
+            twitter = twitter_factory.getInstance();
+
+            Reach obj = new Reach();
+            obj.GetUserTimeline();
+            obj.debug_output();
     }
     
-    public void GetHomeTimeline() throws TwitterException {
-        try {
-            ConfigurationBuilder cb = new ConfigurationBuilder();
-            cb.setDebugEnabled(true)
-                    .setOAuthConsumerKey("***")
-                    .setOAuthConsumerSecret("***")
-                    .setOAuthAccessToken("***")
-                    .setOAuthAccessTokenSecret("***");
-
-            TwitterFactory twitter_factory = new TwitterFactory(cb.build());
-            Twitter twitter = twitter_factory.getInstance();
-            //User me = twitter.verifyCredentials();
-            //ResponseList<Status> statuses = twitter.getHomeTimeline(new Paging((current_page++),current_page_content));
-            /**
-             * This method is better than getHomeTimeline() because it provides
-             * only original tweets
+    public void GetUserTimeline() throws TwitterException {
+            
+            /** ResponseList<Status> statuses = twitter.getHomeTimeline(new Paging((current_page++),current_page_content));
+             * Could work since it uses authenticated user to access their data. 
+             * Does not make a distinction between retweets and the user's original
+             * content. Hence getUserTimeline is better and more flexible. 
              */
             
             int tweets_per_page = 50;
             int current_page = 1;
             ResponseList<Status> statuses = twitter.getUserTimeline("banunu_dog", new Paging(current_page++, tweets_per_page));
             for (Status whatIsaid : statuses) { 
-                user_original_content.add(whatIsaid.getText());
+                user_original_timeline.add(whatIsaid.getText());
             }
             
             /**
@@ -72,19 +74,43 @@ public class Reach {
                     e.printStackTrace();
                 }
             }**/
-            
-        } catch (TwitterException e) {
-            e.printStackTrace();
-            System.err.println("Sorry but failed to get timeline! :(" + e.getErrorMessage());
-            System.exit(-1);
-        }
-
+           
     }
-
+    
+    public String fetchUsername(String status){
+        if(status.contains("@")){
+                //This selection filters out direct mentions as well as retweets
+                String influencer_username = "";
+                for(int x = (status.indexOf('@')+1); x < status.indexOf(' '); x++){
+                    influencer_username += status.charAt(x);
+                }
+            }    
+            else if(status.contains("RT") && (status.contains("@"))){
+                    //This selection filters out retweets only
+                }
+    }
+    
+    public void GetInfluencers() throws TwitterException{
+        /**
+         * TODO: Figure out how this fits in with authentication 
+         * applications. This has been demoed on ambient_memory and
+         * not on banunu_dog. 
+         */
+        /**
+         * This method processes the user's timeline to fetch who the user
+         * retweets the most and who the suer directly mentions the most.  
+         */
+        for(String status: user_original_timeline){
+            
+            }
+        
+        
+    }
+    
     public void debug_output() {
         System.out.println("User's timeline collected is as follows:");
-        for (int count_of_statuses = 0; count_of_statuses < user_original_content.size(); count_of_statuses++) {
-            System.out.println("Debug: " + (count_of_statuses + 1) + ": " + user_original_content.get(count_of_statuses));
+        for (int count_of_statuses = 0; count_of_statuses < user_original_timeline.size(); count_of_statuses++) {
+            System.out.println("Debug: " + (count_of_statuses + 1) + ": " + user_original_timeline.get(count_of_statuses));
         }
     }
 }
