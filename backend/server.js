8229279f87442214294b08b959472570d662dc78
Piyali Mukherjee <piyali.mukherjee.30.pm@gmail.com>
2015-05-20 12:07:02 -0400

    Implemented 3 of 7 metrics, 3 underway
    Still need to debug these as well as find out what is going on with
    those metrics that need me to call tweet.entities JSON property

--- backend/server.js
+++ backend/server.js
@@ -122,12 +122,127 @@ var twit_resp = function (err, data, resp) {
 			return;
 		} else {
 			//Analytics...
-			resp_str =  "The total count of tweets received is : "+tweets.length+"\n"+"The first element is :"+"\n"+JSON.stringify(tweets[0],null,4);
-			resp_ready =  true;
-		};
-		console.log("Debug tweet data pushed : " + tweets.length + " and chunk count : " + data_rcvd_count);
-	}
-	if (resp)
-		console.log("Debug resp also rcvd ");
-	return;
-};
+			var num_retweets = 0;
+			var array_of_tags = [];
+			var array_of_locations =[];
+			var people_user_replies_to = [];
+			var people_user_mentions =[];
+			
+			var is_found = false;
+			var location_exists = false;
+			var responsee_exists = false;
+			
+			for (var i = 0; i < tweets.length; i++) {
+				//Metric number 1: processing the numerator for average number of retweeters here
+				num_retweets += tweets[i].retweet_count;
+				
+				//Metric number 2: This one is for the tracker of hastags
+				/*if(tweets[i].entities.hashtags.length !== 0){
+					//We first check if it already exists in the array of tags
+					is_found = false;
+					for (var k = 0; k < array_of_tags.length; k++) {
+						if (array_of_tags[k].hashtag === tokens_in_text[j].trim()) {
+							array_of_tags[k].count += 1;
+							is_found = true;
+							break;
+						}
+					} //end of for..
+					if (is_found === false) {
+						//Not found we add
+						var new_obj = {'hashtag': tokens_in_text[j].trim(),
+							'count': 1 }
+						array_of_tags.push(new_obj);
+					}
+				}*/
+				
+				//Metric number 3: Where do you tweet from the most?
+				if(tweets[i].geo !== null){
+					location_exists = false;
+					for( var k = 0; k < array_of_locations.length; k++){
+						if (array_of_locations[k].location === tweets[i].geo.trim()) {
+								array_of_locations[k].count += 1;
+								location_exists = true;
+								break;
+							}
+					}//end of for
+					if(location_exists === false){
+						//Never seen this before, so we add them to array of locations
+						var new_location = { 'location': tweets[i].geo.trim(), 'count': 1}
+						array_of_locations.push(new_location);
+					}
+				}
+				
+				//Metric number 4: Who do you reply to the most?
+				if(tweets[i].in_reply_to_screen_name !== null){
+					responsee_exists = false;
+					for( var k = 0; k < people_user_replies_to.length; k++){
+						if (people_user_replies_to[k].person === tweets[i].in_reply_to_screen_name.trim()) {
+								people_user_replies_to[k].count += 1;
+								responsee_exists = true;
+								break;
+							}
+					}//end of for
+					if(responsee_exists === false){
+						//Never seen this before, so we add them to array of locations
+						var new_person = { 'person': tweets[i].in_reply_to_screen_name.trim(), 'count': 1}
+						people_user_replies_to.push(new_location);
+					}
+				}
+				
+				//Metric number 5: Who do you mention the most?
+				/*
+				if(tweets[i].entities.user_mentions !== 0){
+					responsee_exists = false;
+					for( var k = 0; k < people_user_mentions.length; k++){
+							for( var x = 0; x < tweets[i].entities.user_mentions.length; x++){
+								if (people_user_mentions[k].person === tweets[i].entities.user_mentions[x].trim()) {
+									people_user_mentions[k].count += 1;
+									responsee_exists = true;
+									break;
+								}
+							}
+					}//end of for
+					if(responsee_exists === false){
+						//Never seen this before, so we add them to array of locations
+						var new_person = { 'person': tweets[i].in_reply_to_screen_name.trim(), 'count': 1}
+						people_user_replies_to.push(new_location);
+					}
+				}*/
+				
+				//Metric number 6: Which links do you post to the most? Or most recent links?
+				
+				//Metric number 7: Who do you retweet the most?
+				if ((tweets[i].text.indexOf("RT") > -1) && (tweets[i].text.indexOf("@") > -1)){
+					
+				}
+				
+			}//end of large tweet cycle processing
+				
+				//sorting the responses array
+				
+				//sorting the locations array
+				
+				//sorting the "keywords" array
+				
+				//calculating average retweets
+				var avg_retweets = num_retweets / tweets.length;
+				
+				//sorting the mentions array
+				
+				//sorting the replies array
+			
+				//sorting the links array
+				
+				//sorting the retweeters array
+
+				resp_str = "The total count of tweets received is : " + tweets.length + "\n" + "The first element is :" + "\n" + JSON.stringify(tweets[0], null, 4);
+				resp_ready = true;
+			}
+			;
+			console.log("Debug tweet data pushed : " + tweets.length + " and chunk count : " + data_rcvd_count);
+		}
+		if (resp)
+			console.log("Debug resp also rcvd ");
+		return;
+}
+;
