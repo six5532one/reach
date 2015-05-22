157260dae07864949206d8347c056efaa2b45a1d
Piyali Mukherjee <piyali.mukherjee.30.pm@gmail.com>
2015-05-21 22:42:31 -0400

    Final working version
    Skipped two of prev analytics, does not implement the sorted attributes
    yet, though that's a minor change and could be done once testing comes
    back

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,18 @@ var twit_client = new twitter({'consumer_key': process.env.TWITTER_CONSUMER_KEY,
 	'access_token_secret': process.env.TWITTER_ACCESS_TOKEN_SECRET});
 var resp_str = "";
 var tweets = [];
-var user_name;
+var user_name;		//This will be kept as a null string while we are waiting for the first request, but will maintain the value till next request
 var resp_ready = false;
 var data_rcvd_count = 0;
+var debug_str = "";
+var resp_object = {
+	'an1': null,
+	'an2': null,
+	'an3': null,
+	'an4': null,
+	'an5': null
+};
+
 //HTTP server
 var http_server = http.createServer(function (request, response) {
 	//console.log("Debug : HTTP server called asking URL " + request.url + " by method : " + request.method + " at : " + (new Date()));
@@ -37,7 +46,7 @@ var http_server = http.createServer(function (request, response) {
 				user_name = user_name_val;		//Set the username for further processing
 				if (user_name.charAt(0) === '@')
 					user_name = user_name.substring(1); //knock off the starting at symbol if sent
-				//console.log("GET request is with name " + user_name);
+				console.log("GET request is with name " + user_name + " and stat value : " + stat_val);
 				twit_client.get('statuses/user_timeline', {
 					'include_rts': true,
 					'contributor_details': true,
@@ -46,32 +55,38 @@ var http_server = http.createServer(function (request, response) {
 					'screen_name': user_name
 				}, twit_resp);
 				response.writeHead(200, {'Content-Type': 'text/plain'});
-				response.end("OK");
+				response.end("Registered user name : " + user_name + " : OK");
 				return;
 			} else if (stat_val === "1") {	//we have been asked to get the statistics of the username uname
 				//We do two checks, one we reconfirm that the username is indeed the same, and second we check if the data is ready
 				if (user_name_val !== user_name) {
-					//The user name provided has got changed.. to respect Data provacy act, we return an invalid user
+					//The user name provided has got changed.. to respect Data Privacy act, we return an invalid user					
 					response.writeHead(400, {'Content-Type': 'text/plain'});
-					response.end('Invalid User Name provided...');
-					console.log("received invalid user name ");
+					response.end('Invalid User Name provided...orig : ' + user_name + ' and now sent : ' + user_name_val);
+					console.log("Debug : received invalid user name orig : " + user_name + " and now sent : " + user_name_val);
 					return;
 				}
 				if (resp_ready === false) {
 					//The request for stat has come too soon we respond as server not ready
 					response.writeHead(502, {'Content-Type': 'text/plain'});
 					response.end('Need more time to finish data processing for this user...');
-					console.log("received stat request too early... ");
+					console.log("Debug : Received stat request too early... ");
 					return;
 				}
 				//Since at this stage resp_ready is set, we return the resp_str
-				response.writeHead(200, {'Content-Type': 'text/plain'});
-				response.end(resp_str);
+				response.writeHead(200, {'Content-Type': 'application/json'});
+				response.end(resp_object);
 				//We now clear the fields of the previous user...
 				resp_ready = false;
 				resp_str = "";
 				tweets = [];
 				data_rcvd_count = 0;
+				user_name = "";
+				resp_object.an1 = null;
+				resp_object.an2 = null;
+				resp_object.an3 = null;
+				resp_object.an4 = null;
+				resp_object.an5 = null;
 				return;
 			} else if (stat_val === "-1") {
 				//This is an abort request, so we re initialize all arrays and wait for next request
@@ -79,6 +94,7 @@ var http_server = http.createServer(function (request, response) {
 				resp_str = "";
 				tweets = [];
 				data_rcvd_count = 0;
+				user_name = "";
 				response.writeHead(200, {'Content-Type': 'text/plain'});
 				response.end("Stats compilation aborted...");
 			}
@@ -121,149 +137,126 @@ var twit_resp = function (err, data, resp) {
 			}, twit_resp);
 			return;
 		} else {
-			//Analytics...
-			var num_retweets = 0;
+			//Analytics... as we have received all three sets .. the var data_rcvd is reset by the http[ handler when it sends the response back
+			var num_retweets = 0;		//Used by the Analytics 1 : Average number of retweets
 			var array_of_tags = [];
 			var array_of_locations = [];
 			var people_user_replies_to = [];
 			var people_user_mentions = [];
-			var links = []; 
 
 			var is_found = false;
 			var location_exists = false;
 			var responsee_exists = false;
-			var link_exists = false;
+
+			//if (tweets.length > 3) tweets.splice(3);	//knock off all but 0,1 and 2
 
 			for (var i = 0; i < tweets.length; i++) {
-				//Metric number 1: processing the numerator for average number of retweeters here
+				//Metric number 1: Average number of retweets
+
+				//We process the numerator for average number of retweeters in the loop
 				num_retweets += tweets[i].retweet_count;
 
-				//Metric number 2: This one is for the tracker of hastags
+				//Metric number 2: Track hashtags
 				if (tweets[i].entities.hashtags.length !== 0) {
-					//We first check if it already exists in the array of tags
-					is_found = false;
-					for (var m = 0; m < tweets[i].entities.hashtags.length; m++) {
-						for (var k = 0; k < array_of_tags.length; k++) {
-							if (array_of_tags[k].hashtag === tweets[i].entities.hashtags[m].trim()) {
+					//We first check if it already exists in the array of tags					
+					for (var m = 0; m < tweets[i].entities.hashtags.length; m++) {	//for each hashtag in the entities list
+						is_found = false;
+						for (var k = 0; k < array_of_tags.length; k++) {			//against each hashtag in the array_of_hashtags array
+							if (array_of_tags[k].hashtag === tweets[i].entities.hashtags[m].text) {
 								array_of_tags[k].count += 1;
 								is_found = true;
 								break;
 							}
-						} //end of for..
+						} //end of for every existing tag
 						if (is_found === false) {
 							//Not found we add
-							var new_obj = {'hashtag': tweets[i].entities.hashtags[m].trim(),
-								'count': 1}
+							var new_obj = {'hashtag': tweets[i].entities.hashtags[m].text, 'count': 1};
 							array_of_tags.push(new_obj);
 						}
-					}
-				}
+					} //(m) end of for every hashtag in the entry
+				} //end of if a hash tag is in the entities list
 
-					//Metric number 3: Where do you tweet from the most?
-					if (tweets[i].geo !== null) {
-						location_exists = false;
-						for (var k = 0; k < array_of_locations.length; k++) {
-							if (array_of_locations[k].location === tweets[i].geo.trim()) {
-								array_of_locations[k].count += 1;
-								location_exists = true;
-								break;
-							}
-						}//end of for
-						if (location_exists === false) {
-							//Never seen this before, so we add them to array of locations
-							var new_location = {'location': tweets[i].geo.trim(), 'count': 1}
-							array_of_locations.push(new_location);
-						}
-					}
-
-					//Metric number 4: Who do you reply to the most?
-					if (tweets[i].in_reply_to_screen_name !== null) {
+				//Metric number 3: Who do you mention the most?
+				if (tweets[i].entities.user_mentions.length !== 0) {
+					for (var m = 0; m < tweets[i].entities.user_mentions.length; m++) {
 						responsee_exists = false;
-						for (var k = 0; k < people_user_replies_to.length; k++) {
-							if (people_user_replies_to[k].person === tweets[i].in_reply_to_screen_name.trim()) {
-								people_user_replies_to[k].count += 1;
+						for (var k = 0; k < people_user_mentions.length; k++) {
+							if (people_user_mentions[k].person === tweets[i].entities.user_mentions[m].screen_name) {
+								people_user_mentions[k].count += 1;
 								responsee_exists = true;
 								break;
 							}
-						}//end of for
+						}	//end of for
 						if (responsee_exists === false) {
 							//Never seen this before, so we add them to array of locations
-							var new_person = {'person': tweets[i].in_reply_to_screen_name.trim(), 'count': 1}
-							people_user_replies_to.push(new_location);
+							var new_person = {'person': tweets[i].entities.user_mentions[m].screen_name, 'count': 1};
+							people_user_mentions.push(new_person);
 						}
-					}
-
-					//Metric number 5: Who do you mention the most?
-					if (tweets[i].entities.user_mentions.length !== 0) {
-						responsee_exists = false;
-						for (var m = 0; m < tweets[i].entities.hashtags.length; m++) {
-							for (var k = 0; k < people_user_mentions.length; k++) {
-								if (people_user_mentions[k].person === tweets[i].entities.user_mentions[m].trim()) {
-									people_user_mentions[k].count += 1;
-									responsee_exists = true;
-									break;
-								}
-							}	//end of for
-							if (responsee_exists === false) {
-								//Never seen this before, so we add them to array of locations
-								var new_person = {'person': tweets[i].in_reply_to_screen_name.trim(), 'count': 1}
-								people_user_replies_to.push(new_location);
-							}
-						}
-					}
-					
-					//Metric number 6: Which links do you post to the most? Or most recent links?
-					if (tweets[i].entities.urls.length !== 0) {
-						link_exists = false;
-						for (var m = 0; m < tweets[i].entities.urls.length; m++) {
-							for (var k = 0; k < links.length; k++) {
-								if (links[k].url === tweets[i].entities.urls[m]) {
-									links[k].count += 1;
-									link_exists = true;
-									break;
-								}
-							}	//end of for
-							if (link_exists === false) {
-								//Never seen this before, so we add them to array of locations
-								var new_link = {'url': tweets[i].in_reply_to_screen_name.trim(), 'count': 1}
-								links.push(new_location);
-							}
-						}
-					}
-					
-					//Metric number 7: Who do you retweet the most?
-					if ((tweets[i].text.indexOf("RT") > -1) && (tweets[i].text.indexOf("@") > -1)) {
-
 					}
 				}
 
-				}//end of large tweet cycle processing
+				//Metric number 4: Who do you reply to the most?
+				if (tweets[i].in_reply_to_screen_name !== null) {
+					responsee_exists = false;
+					for (var k = 0; k < people_user_replies_to.length; k++) {
+						if (people_user_replies_to[k].screen_name === tweets[i].in_reply_to_screen_name.trim()) {
+							people_user_replies_to[k].count += 1;
+							responsee_exists = true;
+							break;
+						}
+					}//end of for
+					if (responsee_exists === false) {
+						//Never seen this before, so we add them to array of reply persons
+						var new_person = {'screen_name': tweets[i].in_reply_to_screen_name.trim(), 'count': 1};
+						people_user_replies_to.push(new_person);
+					}
+				}
 
-				//sorting the responses array
+				//Metric number 5: Where do you tweet from the most?
 
-				//sorting the locations array
+				if (tweets[i].place !== null) {
+					location_exists = false;
+					for (var k = 0; k < array_of_locations.length; k++) {
+						if (array_of_locations[k].location === tweets[i].place.full_name) {
+							array_of_locations[k].count += 1;
+							location_exists = true;
+							break;
+						}
+					}//end of for
+					if (location_exists === false) {
+						//Never seen this before, so we add them to array of locations
+						var new_location = {'location': tweets[i].place.full_name, 'count': 1};
+						array_of_locations.push(new_location);
+					}
+				}
 
-				//sorting the "keywords" array
 
-				//calculating average retweets
-				var avg_retweets = num_retweets / tweets.length;
+				/*Metric number 6: Who do you retweet the most?
+				 if ((tweets[i].text.indexOf("RT") > -1) && (tweets[i].text.indexOf("@") > -1)) {
+				 
+				 }
+				 */
 
-				//sorting the mentions array
+			}//end of large tweet cycle processing
+			//Now we do the final analysis of the data extracted
+			var avg_retweets = num_retweets / tweets.length;
 
-				//sorting the replies array
+			resp_object.an1 = avg_retweets;
+			resp_object.an2 = array_of_tags;
+			resp_object.an3 = people_user_mentions;
+			resp_object.an4 = people_user_replies_to;
+			resp_object.an5 = array_of_locations;
+			//resp_str = "First object :\n"+JSON.stringify(tweets[0],null,4)+"\nSecond object :\n"+JSON.stringify(tweets[1],null,4)+"\n";//Third object :\n"+JSON.stringify(tweets[2],null,4)+"\n";
+			//resp_str = "Debug str :\n"+
+			resp_str = JSON.stringify(resp_object);//+"\n"+debug_str;
+			resp_ready = true;
 
-				//sorting the links array
-
-				//sorting the retweeters array
-
-				resp_str = "The total count of tweets received is : " + tweets.length + "\n" + "The first element is :" + "\n" + JSON.stringify(tweets[0], null, 4);
-				resp_ready = true;
-			}
-			;
-			console.log("Debug tweet data pushed : " + tweets.length + " and chunk count : " + data_rcvd_count);
 		}
-		if (resp)
-			console.log("Debug resp also rcvd ");
-		return;
-}
-;
+		; //end of else
+		console.log("Debug tweet data pushed : " + tweets.length + " and chunk count : " + data_rcvd_count);
+	}
+	;
+	if (resp)
+		console.log("Debug resp also rcvd ");
+	return;
+}; //end of twit_resp call back funtion
